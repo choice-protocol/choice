e8e92f9a08c5cef193e68b08d5042b930436a179
Harper Reed <harper@nata2.org>
2021-05-11 16:17:39 -0500

    added alchemy

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ func getProxyUrl() string {
 
 	// put logic in here that chooses the proxy
 
-	default_condition_url := "https://mainnet.infura.io/v3/c5b349fd47244da8a4df10652b911d38"
+	default_condition_url := "https://eth-mainnet.alchemyapi.io/v2/ikJ14RMH8ZjS-H0F3QUOd-lwec5TzkcV/"
 
 	return default_condition_url
 }
